7e2bb45656bed23cbac60ec2ac2230ecac14b7ab
ramazanerikli <ramazanerikli1@gmail.com>
2021-05-04 08:09:49 +0200

    Remove timer

--- src/App.js
+++ src/App.js
@@ -5,21 +5,6 @@ function App() {
   // Breathe message
   const [message, setMessage] = useState('breathe in');
   
-  // Time
-  const [time, setTime] = useState(60);
-  let seconds = time % 60;
-  let minutes = Math.floor(time / 60);
-  minutes = minutes.toString().length === 1 ? "0" + minutes : minutes;
-  seconds = seconds.toString().length === 1 ? "0" + seconds : seconds;
-
-  useEffect(() => { 
-    if (time > 0) {
-      setTimeout(() => setTime(time - 1), 1000);
-    } else {
-      clearInterval();
-    }
-  });
-
   useEffect(() => {
     const interval = setInterval(() => {
       setMessage(message => message === 'breathe in' ? 'breathe out' : 'breathe in');
@@ -29,7 +14,6 @@ function App() {
 
   return (
     <div className="App">
-      <span className="timer">{minutes + ':' + seconds}</span>
       <div className={"circle " + (message === 'breathe in' ? 'grow' : 'shrink')}>
         <div className="circle-inner">
           <span>{message}</span> 
